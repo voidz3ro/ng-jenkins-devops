fc30123b68c118283865ab7f913fae3b313f39fd
Tristan Bullard <tristan.bullard@gmail.com>
2018-10-29 15:34:32 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DynamicFormsCoreModule } from "@ng-dynamic-forms/core";
 
 import { AppComponent } from './app.component';
+import { ReportExportComponent } from './report-export/report-export.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormsKendoUIModule } from '@ng-dynamic-forms/ui-kendo';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportExportComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    DynamicFormsCoreModule.forRoot(),
+    DynamicFormsKendoUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
