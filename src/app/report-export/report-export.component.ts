9a8708262906657f2779923e24d0ee0c13431542
Tristan Bullard <tristan.bullard@gmail.com>
2018-10-30 10:28:31 -0500

    checking on jenkins git build hoo

---
+++ src/app/report-export/report-export.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-export',
+  templateUrl: './report-export.component.html',
+  styleUrls: ['./report-export.component.css']
+})
+export class ReportExportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
